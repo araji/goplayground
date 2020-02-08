0ffe1b3e9eaac4dcef50dd8dd4739e58999afe8d
Abdelaziz Raji <abdelaziz.raji@gmail.com>
2020-02-07 20:52:34 -0500

    intro to run and unicode package

--- exercism/go/isogram/isogram.go
+++ exercism/go/isogram/isogram.go
@@ -1,1 +1,22 @@
 package isogram
+
+import (
+	"unicode"
+)
+
+func IsIsogram(s string) bool {
+	var amap = make(map[rune]int)
+	for _, char := range s {
+		if !unicode.IsLetter(char) {
+			continue
+		}
+		char = unicode.ToLower(char)
+		_, ok := amap[char]
+		if ok {
+			return false
+		} else {
+			amap[char] = 1
+		}
+	}
+	return true
+}
