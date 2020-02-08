95e86c6ba17afccd694c4d8061e1bed90f80ce0c
Abdelaziz Raji <abdelaziz.raji@gmail.com>
2020-02-08 13:32:21 -0500

    reverse unicode string

--- exercism/go/reverse-string/reverse_string.go
+++ exercism/go/reverse-string/reverse_string.go
@@ -1,1 +1,32 @@
 package reverse
+
+/**import (
+	"fmt"
+
+)
+*/
+func Reverse(in string) string {
+	s1 := []rune(in)
+	s2 := []rune{}
+	l := len(s1)
+	for i:=0; i< l ; i++ {
+		///s2= append(s2,s1[l-i-1:l-i]...)
+		// or
+	    s2= append(s2,s1[l-i-1:l-i][0])
+		
+	}
+	return string(s2)
+}
+
+
+
+//Fails on multibyte support 
+func Reverse1(in string) string {
+	l := len(in)
+	out := ""
+	for i:=0; i< l ; i++ {
+		c:= in[l-i-1:l-i]
+		out += c
+	}
+	return out
+}
