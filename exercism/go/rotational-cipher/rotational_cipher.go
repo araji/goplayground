875e0788cc21c9da1aad828dc737272d0828714c
araji <abdelaziz.raji@gmail.com>
2020-02-08 22:13:51 -0500

    rot13

--- exercism/go/rotational-cipher/rotational_cipher.go
+++ exercism/go/rotational-cipher/rotational_cipher.go
@@ -1,1 +1,23 @@
 package rotationalcipher
+
+import (
+	"strings"
+)
+
+
+// RotationalCipher same as simpleCipher except for case sensitivity and keeping non alpha.
+// as described in strings.Map example :-).
+func RotationalCipher(message string, shiftKey int ) string {
+	
+	rot13:= func(r rune) rune {	
+		switch {
+		case r >= 'A' && r <= 'Z':
+			return 'A' + (r-'A'+ rune(shiftKey))%26
+		case r >= 'a' && r <= 'z':
+			return 'a' + (r-'a'+ rune(shiftKey) )%26
+		}
+		return r
+	}
+	
+	return strings.Map(rot13,message)
+}
