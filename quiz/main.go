b166d3c9559894dfb4fdded0cc03be19b60194ed
Abdelaziz Raji <abdelaziz.raji@gmail.com>
2020-02-09 19:01:45 -0500

    add timer , wip with constraints : no goto label and stream reading required (no readall()

--- quiz/main.go
+++ quiz/main.go
@@ -17,23 +17,28 @@ var (
 	limit    int
 )
 
+/**
 func timer(timeout int, done chan bool) {
 	tstart := time.Now()
-
+	fmt.Printf("Timer of %d started \n", timeout)
 	for {
 		if time.Now().Sub(tstart).Seconds() >= float64(timeout) {
 			break
+
 		}
-		done <- false
+		//done <- false
 	}
-	fmt.Println("TIME's UP ")
+	fmt.Println("TIME's UP\n ")
 	done <- true
 }
+
+*/
 func main() {
 	flag.StringVar(&filename, "filename", "problems.csv", "exercise filename")
 	flag.IntVar(&limit, "limit", 10, "time limit")
 	flag.Parse()
-	done := make(chan bool)
+	//done := make(chan bool)
+	answerChan := make(chan bool)
 	//read csv file
 	fid, err := os.Open(filename)
 	if err != nil {
@@ -43,21 +48,18 @@ func main() {
 	csvReader := csv.NewReader(fid)
 	right := 0
 	wrong := 0
-
 	fmt.Fprintf(os.Stdout, "Hit Enter to start")
 	reader := bufio.NewReader(os.Stdin)
 	_, err = reader.ReadString('\n')
 	if err != nil {
 		log.Fatalf("%s", err)
 	}
-	go timer(limit, done)
-	tstart := time.Now()
 
+	timer1 := time.NewTimer(time.Duration(limit * int(time.Second)))
+	defer timer1.Stop()
+
+	tstart := time.Now()
 	for {
-		if <-done {
-			fmt.Println("received done message")
-			break
-		}
 		record, err := csvReader.Read()
 		if err == io.EOF {
 			break
@@ -65,18 +67,29 @@ func main() {
 		if err != nil {
 			log.Fatalf("%s", err)
 		}
-		if quiz(strings.TrimSpace(record[0]), strings.TrimSpace(record[1])) {
-			right++
-		} else {
-			wrong++
+		go quiz(strings.TrimSpace(record[0]), strings.TrimSpace(record[1]), answerChan, *timer1)
+		select {
+		case <-timer1.C:
+			fmt.Println("TIMEOUT RECEIVED")
+			break
+		case msg2 := <-answerChan:
+			fmt.Println("ANSWER RECEIVED")
+			if msg2 == true {
+				right++
+			} else {
+				wrong++
+			}
 		}
+
 	}
+	//fmt.Println("Out of Select")
+	//break
+
 	tend := time.Now()
 	fmt.Printf("Total score : correct = %d , wrong = %d  was achieved under %.2f seconds\n", right, wrong, time.Duration(tend.Sub(tstart)).Seconds())
 }
 
-func quiz(question, expected string,done channel bool) bool {
-	
+func quiz(question, expected string, answerChan chan<- bool, timeout time.Timer) {
 	fmt.Fprintf(os.Stdout, "%s =  ", question)
 	reader := bufio.NewReader(os.Stdin)
 	answer, err := reader.ReadString('\n')
@@ -85,8 +98,10 @@ func quiz(question, expected string,done channel bool) bool {
 	}
 	answer = strings.TrimSpace(answer)
 	if answer != expected {
-		fmt.Printf("Expected %s but got %s\n", expected, answer)
-		return false
+		//fmt.Printf("Expected %s but got %s\n", expected, answer)
+		answerChan <- false
+	} else {
+		answerChan <- true
 	}
-	return true
+
 }
