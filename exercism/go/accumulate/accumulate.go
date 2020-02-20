83a1eb04bb6ccd0354a265973ef5d9c65bad2b68
Abdelaziz Raji <abdelaziz.raji@gmail.com>
2020-02-20 05:26:47 -0500

    accumulate: from other solutions : cool named return

--- exercism/go/accumulate/accumulate.go
+++ exercism/go/accumulate/accumulate.go
@@ -1,10 +1,10 @@
 package accumulate
 
 // Accumulate apply operation to each element of input
-func Accumulate(s []string, op func(string) string) []string {
-	out := make([]string, 0, 1)
+func Accumulate(s []string, op func(string) string) (out []string) {
+	//out = make([]string, 0, 1)
 	for _, c := range s {
 		out = append(out, op(c))
 	}
-	return out
+	return
 }
